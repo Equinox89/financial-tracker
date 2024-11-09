35e583843b24ffbb0a2a596415a9de3f9d0a48d9
Oleh <mastercrutor89@gmail.com>
2024-11-09 15:47:15 +0200

    Transaction view

--- pages/index.vue
+++ pages/index.vue
@@ -13,6 +13,12 @@
     <Trend color="green" title="Investments" :amount="4000" :last-amount="3000" :loading="false" />
     <Trend color="red" title="Savings" :amount="4000" :last-amount="4100" :loading="false" />
   </section>
+  <section>
+    <Transaction />
+    <Transaction />
+    <Transaction />
+    <Transaction />
+  </section>
 </template>
 
 <script setup lang="ts">
