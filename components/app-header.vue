1bdba92905988417b08e3b6189f47ee4d7b6c915
equinox89 <mastercrutor89@gmail/com>
2024-12-28 13:08:12 +0200

    Authentication: Sign-In Logic and Magic Links

--- components/app-header.vue
+++ components/app-header.vue
@@ -8,3 +8,11 @@
     </div>
   </header>
 </template>
+<script setup>
+  const user = useSupabaseUser()
+  watch(user, (user) => {
+    if (user) {
+      console.log(user)
+    }
+  }, { immediate: true })
+</script>
