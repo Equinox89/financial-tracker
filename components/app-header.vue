432379765bd4809d31a66d5620a1faff088a649a
equinox89 <mastercrutor89@gmail/com>
2024-12-28 14:27:20 +0200

    Signed In User Data & Signing Out

--- components/app-header.vue
+++ components/app-header.vue
@@ -19,7 +19,7 @@
         </template>
 
         <template #item="{ item }">
-          <span class="truncate">{{ item.label }}</span>
+          <span class="truncate" @click="item.onClick">{{ item.label }}</span>
 
           <UIcon :name="item.icon" class="flex-shrink-0 h-4 w-4 text-gray-400 dark:text-gray-500 ms-auto" />
         </template>
@@ -31,22 +31,29 @@
 <script setup>
 const supabase = useSupabaseClient()
 const user = useSupabaseUser()
-const items = [
-  [{
-    label: user.value?.email,
-    slot: 'account',
-    disabled: true
-  }], [{
-    label: 'Settings',
-    icon: 'i-heroicons-cog-8-tooth',
-    onClick: () => console.log('Link to settings in the future')
-  }, {
-    label: 'Sign out',
-    icon: 'i-heroicons-arrow-left-on-rectangle',
-    onClick: async () => {
-      await supabase.auth.signOut()
-      return navigateTo('/login')
+const items = ref([
+  [
+    {
+      label: user.value?.email,
+      slot: 'account',
+      disabled: true
     }
-  }]
-]
+  ],
+  [
+    {
+      label: 'Settings',
+      icon: 'i-heroicons-cog-8-tooth',
+      onClick: () => console.log('Link to settings in the future')
+    },
+    {
+      label: 'Sign out',
+      icon: 'i-heroicons-arrow-left-on-rectangle',
+      onClick: async () => {
+        console.log('click sign out')
+        await supabase.auth.signOut()
+        return navigateTo('/login')
+      }
+    }
+  ]
+])
 </script>
--- composables/useRedirectIfAuthenticated.js
+++ composables/useRedirectIfAuthenticated.js
@@ -1,10 +1,9 @@
-export const useRedirectIfAuthenticated = (url ='/') => {
+export const useRedirectIfAuthenticated = (url = '/') => {
     const user = useSupabaseUser()
     watch(user, (user) => {
         if (user) {
-            console.log(user)
+            return navigateTo(url)
         }
     }, { immediate: true })
-
-    return user
+    return { user }
 }
